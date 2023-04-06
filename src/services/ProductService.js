368ea9a4dbba55902c6184003f788cf6224d1e60
touria2001 <77863089+touria2001@users.noreply.github.com>
2023-04-06 22:43:52 +0000

    deploy

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://product-database-35px.onrender.com',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
